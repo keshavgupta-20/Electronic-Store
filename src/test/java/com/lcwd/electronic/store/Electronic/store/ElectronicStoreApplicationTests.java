f4fa4337b5ea7aef30b26570beb5daed409711ae
gupta <guptakeshav200920032@GMAIL.COM>
2025-07-06 22:05:21 +0530

    hey

--- src/test/java/com/lcwd/electronic/store/Electronic/store/ElectronicStoreApplicationTests.java
+++ src/test/java/com/lcwd/electronic/store/Electronic/store/ElectronicStoreApplicationTests.java
@@ -1,13 +1,34 @@
 package com.lcwd.electronic.store.Electronic.store;
 
+import com.lcwd.electronic.store.Electronic.store.Entites.User;
+import com.lcwd.electronic.store.Electronic.store.repositoreis.UserRepo;
+import com.lcwd.electronic.store.Electronic.store.security.JwtHelper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
 
 @SpringBootTest
 class ElectronicStoreApplicationTests {
+	@Autowired
+	private UserRepo userRepo;
 
+	@Autowired
+	private JwtHelper jwtHelper;
 	@Test
 	void contextLoads() {
 	}
-
+	@Test
+	void testToken(){
+		User user = userRepo.findByEmail("keshav@gmail.com").get();
+		String token = jwtHelper.generateToken(user);
+		System.out.println(token);
+		System.out.println("Testing JWT token");
+		System.out.println(jwtHelper.getUserFromToken(token));
+		System.out.println(jwtHelper.isTokenExpired(token));
+		System.out.println(jwtHelper.getExpirationDateFromToken(token));
+//		System.out.println("Testing jwt Token");
+		System.out.println();
+	}
 }
